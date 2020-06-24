f794fad8d2d09b8f237fbb2cc6219c4a6322ac67
Mohammed <tuniis@live.com>
2020-06-23 23:06:27 -0500

    Updated nuget packages. Added dbContaxt and migration.

---
+++ Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoApi.Data
+{
+    using DemoApi.Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DemoApi-local;Trusted_Connection=True;MultipleActiveResultSets=true");
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoApi.Domain.Entities
+{
+    public class User
+    { 
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
